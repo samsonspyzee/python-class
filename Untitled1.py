9df06dc92465d9c745d5ec1f9b27974f038397ef
EC2 Default User <ec2-user@ip-10-16-10-186.us-west-2.compute.internal>
2023-06-15 15:22:08 +0000

    initial commit

---
+++ Untitled1.py
@@ -0,0 +1,23 @@
+print("Hello, World")
+print("Python has three numeric types: int, float, and complex")
+myValue=1.5
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myString = "This is a string."
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+clear
